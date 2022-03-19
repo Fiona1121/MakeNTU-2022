c65111e415bf824f64848ba94596dfe544e0c35a
傅譽的MacBook <b08901039@ntu.edu.tw>
2022-03-20 04:31:25 +0800

    remove redundant detector

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 # Model detection
-pip uninstall keras -y
-pip uninstall keras-nightly -y
-pip uninstall keras-Preprocessing -y
-pip uninstall keras-vis -y
-pip uninstall tensorflow -y
-pip uninstall h5py -y
-pip install tensorflow-gpu==1.13.1
-pip install keras==2.0.8
-pip install h5py==2.10.0
+pip3 uninstall keras -y
+pip3 uninstall keras-nightly -y
+pip3 uninstall keras-Preprocessing -y
+pip3 uninstall keras-vis -y
+pip3 uninstall tensorflow -y
+pip3 uninstall h5py -y
+pip3 install tensorflow-gpu==1.13.1
+pip3 install keras==2.0.8
+pip3 install h5py==2.10.0
 
 # RPisocket
-pip install websocket-client
+pip3 install websocket-client
